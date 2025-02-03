ab14d4aa21e2854852b5fa455b9d953a6147d01f
zahid-iqbal-alpha <163305499+zahid-iqbal-alpha@users.noreply.github.com>
2025-02-03 12:50:43 +0500

    Update grapple-go-cli.rb

--- Formula/grapple-go-cli.rb
+++ Formula/grapple-go-cli.rb
@@ -6,7 +6,7 @@ class GrappleGoCli < Formula
   desc "Command-line interface for Grapple"
   homepage "https://github.com/grapple-solution/grapple-go-cli"
 
-  url "https://github.com/grapple-solution/grapple-go-cli/archive/refs/tags/0.1.1.tar.gz"
+  url "https://github.com/grapple-solution/grapple-go-cli/archive/refs/tags/0.0.1.tar.gz"
 
   depends_on "go" => :build
 
