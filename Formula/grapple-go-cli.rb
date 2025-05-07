89ccb82e974ed70c4ca31d8af03452ecb6dae155
zahid-iqbal-alpha <163305499+zahid-iqbal-alpha@users.noreply.github.com>
2025-05-07 10:43:42 +0500

    Update grapple-go-cli.rb

--- Formula/grapple-go-cli.rb
+++ Formula/grapple-go-cli.rb
@@ -1,28 +1,26 @@
 class GrappleGoCli < Formula
   desc "Command-line interface for Grapple"
   homepage "https://github.com/grapple-solution/grapple-go-cli"
-
   url "https://github.com/grapple-solution/grapple-go-cli/archive/refs/tags/0.0.5.tar.gz"
-
   depends_on "go" => :build
-
+  
   def install
-    system "go", "build", "-o", "grapple-test", "main.go"
-    bin.install "grapple-test"
-
-
+    system "go", "build", "-o", "grapple", "main.go"
+    bin.install "grapple"
     # Install additional required directories
     (share/"grapple-go-cli").install "template-files"
     (share/"grapple-go-cli").install "files"
+    # Install VERSION file
+    (share/"grapple-go-cli").install "VERSION"
   end
-
+  
   def caveats
     <<~EOS
-      Run 'grapple help' to check the list of avaiable commands
+      Run 'grapple help' to check the list of available commands
     EOS
   end
-
+  
   test do
-    system "#{bin}/grapple-test", "help"
+    system "#{bin}/grapple", "help"
   end
 end
